f4944485d5112d58ab7a2d6d82e965044bdaa493
natdanxi <natdanai021046@gmail.com>
2026-06-21 09:50:21 +0700

    Remove DIRECT_URL from frontend

--- src/api.js
+++ src/api.js
@@ -1,3 +1,2 @@
 // src/api.js
 export const API_URL = 'https://food-qk08.onrender.com';
-DIRECT_URL="postgresql://postgres.jawqinianzwdylakzckm:pong1234@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres"
